cf86bb9889e0f1284573cb977aa355f77cb6d654
cverges1 <122914738+cverges1@users.noreply.github.com>
2023-07-20 11:47:40 -0700

    completed models and seeded data for testing

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,10 @@
-const Sequilize = require('sequelize');
 require('dotenv').config();
+const Sequelize = require('sequelize');
 
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
-    sequelize = new Sequilize(process.env.JAWSDB_URL);
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
     sequelize = new Sequelize(
 		process.env.DB_NAME,
