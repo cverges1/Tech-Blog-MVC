8f18104fdb69191369a41bb82740729eb6e145a9
cverges1 <122914738+cverges1@users.noreply.github.com>
2023-07-24 11:24:48 -0700

    updated views and with mutiple pages and functionality

---
+++ public/js/signup.js
@@ -0,0 +1,30 @@
+const signupFormHandler = async (event) => {
+	event.preventDefault();
+
+	// collect user input
+	const username = document.querySelector('#username').value;
+	const userEmail = document.querySelector('#user-email').value;
+	const userPassword = document.querySelector('#user-password').value;
+
+	console.log(username,userEmail,userPassword);
+
+	const response = await fetch('/api/users/', {
+		method: 'POST',
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify({
+			username: username,
+			email: userEmail,
+			password: userPassword,
+		}),
+	});
+
+	if (response.ok) {
+		document.location.replace('/');
+	} else {
+		alert(response.statusText);
+	}
+};
+
+document
+.querySelector('#signup-form')
+.addEventListener('submit', signupFormHandler);
